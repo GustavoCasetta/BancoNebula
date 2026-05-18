efb8664210d72d40000166a68186375851dca79a
Gustavo Casetta <gustavo.casetta@gmail.com>
2026-05-18 08:09:09 -0300

    Incrementando código
    Adicionando o arquivo "usuarios.txt" para guardar os cadastros

--- banco.php
+++ banco.php
@@ -1,101 +1,112 @@
 <?php
-
 // Mini sistema bancário  ©Gustavo Casetta
-echo "Qual seu nome de usuário?\n";  // Pede o nome do usuário
-$nome = (string) trim(fgets(STDIN));  // Grava o nome na variável
+
+require __DIR__ . "/app.php";
+
+echo "Banco Matriz\n";
+echo "1 - Entrar\n2 - Cadastrar\n0 - Sair\nDigite uma opção: ";
+$opcaoLogin = (int) trim(fgets(STDIN));
 $saldo = 0; // variável inicia em 0 (zero)
 $extrato = [];
 
-function exibeMenu($nome){ // Função para exibir o menu
-echo "\n------------------------\n";
-echo "Bem-vindo(a) " . $nome . "!\n";
-echo "------------------------\n";
-}
-
-function saldoBancario($saldo){ // Função para exibir o saldo atual
-    echo "Seu saldo é de: R$ " . number_format($saldo, 2, ",", ".") . "\n";
-    return $saldo;
-}
-
-function depositoBancario($saldo, $extrato){ // Função para realizar depósitos
-    echo "Digite o valor que deseja depositar:\n";
-    $valorDeposito = (float) trim(fgets(STDIN));
-    if($valorDeposito <= 0){
-        echo "Erro! O valor não pode ser negativo!\n";
-    } else {
-    $saldo += $valorDeposito;
-    echo "Deposito realizado com sucesso!\n";
-    $extrato[] = "+ R$ " . number_format($valorDeposito, 2, ",", ".") . " (Depósito)";
-    }
-    return [$saldo, $extrato];
-   
-    }
-
-function saqueBancario($saldo, $extrato){ // Função para realizar saques
-    echo "Digite o valor que deseja sacar:\n";
-    $valorSaque = (float) trim(fgets(STDIN));
-    if($valorSaque > $saldo || $valorsaque < 0){
-        echo "Erro! Saque inválido!\n";
-    } else {
-        $saldo -= $valorSaque;
-        echo "Saque realizado! Aguarde o dinheiro está sendo contado!\n";
-        $extrato[] = "- R$ " . number_format($valorSaque, 2, ",", ".") . " (Saque)";
-    }
-    return [$saldo, $extrato];
-}
-
-function limparTela(){  // Função que limpa a tela após o usuário digitar uma opção
-    if (PHP_OS_FAMILY === 'Windows') {
-        system('cls');
-    } else {
-        system('clear');
-    }
-}
-
-do{ // Loop principal
-    exibeMenu($nome); // Chama a função para exibir o menu
-
-    echo "Opções:\n"; // Mostra as opções
-    echo "1 - Saldo\n2 - Depositar\n3 - Sacar\n4 - Ver extrato\n5 - Sair\nDigite a opção: ";
-    $opcao = (int) trim(fgets(STDIN)); // Grava a opção escolhida na variável
-    limparTela(); // Chama a função para limpar a tela
-
-    switch($opcao){ // Switch das opções
+do{
+    switch($opcaoLogin){
         case 1:
-            saldoBancario($saldo); // Chama a função para mostrar o saldo
-        break;
+            $loginOk = false; // Variável para verificar se login está correto, começa como "false"
+            echo "Insira os dados para efetuar o login\n";
+            echo "Usuário: ";
+            $usuarioLogin = (string) trim(fgets(STDIN)); // Usuário insere nome
+            echo "Senha: ";
+            $senhaLogin = (string) trim(fgets(STDIN)); // Usuário insere senha
 
-        case 2:
-            list ($saldo, $extrato) = depositoBancario($saldo, $extrato); // Chama a função para realizar o depósito
-        break;
-
-        case 3:
-            list ($saldo, $extrato) = saqueBancario($saldo, $extrato); // Chama a função para realizar o saque
-        break;
-
-        case 4: // Mostra o extrato bancário
-            echo "\n------------------------\n";
-            echo "EXTRATO\n";
-            echo "-------------------------\n";
-            if(empty($extrato)){
-                echo "Nenhuma movimentação foi realizada!\n";
-            } else {
-                foreach($extrato as $mov){
-                    echo $mov . "\n";
-                }
-                echo "Saldo autal: R$ " . number_format($saldo, 2, ",", ".") . "\n";
+            if(!file_exists(__DIR__ . "/usuarios.txt")){ // Verifica se existe o arquivo onde se encontram os cadastros
+                echo "Nenhum usuário cadastrado!\n";
+                break;
             }
-        break;
 
-        case 5: // Sai do programa
-            echo "Saindo...";
-        break;
+            $cadastroUsuario = fopen(__DIR__ . "/usuarios.txt", "a"); // Abre o arquivo e o coloca em uma variável
 
+            foreach($cadastroUsuario as $nomes){ // Verifica cada nome e senha para ver se o login está correto
+
+                list($loginArquivo, $senhaArquivo) = explode(";", trim($nomes));
+
+                if($loginArquivo === $usuarioLogin && $senhaArquivo === $senhaLogin){
+                    $loginOk = true; // Se o login estiver correto a variável do login muda para true
+                    break;
+                }
+            }
+
+            if($loginOk === false){ // Verifica se a varíavel do login é false ou true
+                echo "Login incorreto!\n";
+                break;
+            }
+
+            fclose($cadastroUsuario); // Fecha o arquivo
+
+                do{
+                    exibeMenu($usuarioLogin); // Chama a função para exibir o menu
+
+                    echo "Opções:\n"; // Mostra as opções
+                    echo "1 - Saldo\n2 - Depositar\n3 - Sacar\n4 - Ver extrato\n5 - Sair\nDigite a opção: ";
+                    $opcaoMenu = (int) trim(fgets(STDIN)); // Grava a opção escolhida na variável
+                    limparTela(); // Chama a função para limpar a tela
+                    
+                    switch($opcaoMenu){ // Switch das opções
+                        case 1:
+                            saldoBancario($saldo); // Chama a função para mostrar o saldo
+                        break;
+
+                        case 2:
+                            list ($saldo, $extrato) = depositoBancario($saldo, $extrato); // Chama a função para realizar o depósito
+                        break;
+
+                        case 3:
+                            list ($saldo, $extrato) = saqueBancario($saldo, $extrato); // Chama a função para realizar o saque
+                        break;
+
+                        case 4:
+                        ($saldo, $extrato) = mostrarExtrato($saldo, $extrato); // Chama a função que mostra o extrato bancário
+                    break;
+
+                    case 5: // Sai do programa
+                        echo "Saindo...";
+                    break;
+
+                    default:
+                        echo "Opção inválida/inexistente! Verifique!\n"; // Caso uma opção inválida seja enviada
+                    break;
+                }
+
+            } while ($opcaoMenu != 5);  // o loop para quando o usuário digitar 5 (zero)
+
+        break;
+        case 2:
+            $cadastroUsuario = fopen(__DIR__ . "/usuarios.txt", "a");
+            echo "Olá! Vamos efetuar seu cadastro!\n";
+            echo "Insira um nome de usuário: ";
+            $nomeUsuario = (string) trim(fgets(STDIN));
+            echo "Informe uma senha: ";
+            $senhaUsuario = (string) trim(fgets(STDIN));
+            echo "Confirme a senha: ";
+            $confirmacaoDeSenha = (string) trim(fgets(STDIN));
+           
+            if($senhaUsuario !== $confirmacaoDeSenha){
+                echo "As senhas não conferem!\n";
+                break;
+            }
+
+            fwrite($cadastroUsuario, $nomeUsuario . ";" . $senhaUsuario . "\n");
+
+            fclose($cadastroUsuario);
+            echo "Usuário cadastrado!\n";
+        break;
+        case 0:
+            echo "Saindo...\n";
+        break;
         default:
-            echo "Opção inválida/inexistente! Verifique!\n"; // Caso uma opção inválida seja enviada
+            echo "Opção inválida!\n";
         break;
     }
-
-} while ($opcao != 5);  // o loop para quando o usuário digitar 0 (zero)
+}while($opcaoLogin != 0);
 
 ?>
